cdccab81815f17797f7ba6612fcd6b052c7bf3b0
washingtonguilhardes <washington.guilhardes@gmail.com>
2024-07-01 14:30:45 -0300

    chore: correcao na infra do projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderItemModule } from './order-item/order-item.module';
+import { OrdersModule } from './orders/orders.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    OrdersModule,
+    UsersModule,
+    OrderItemModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
