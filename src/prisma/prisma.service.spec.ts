730fb645f5f21c7fa1216f44297c0cd9e5a0468e
washingtonguilhardes <washington.guilhardes@gmail.com>
2024-07-01 23:54:14 -0300

    feat: adicionado mais casos de teste

--- src/prisma/prisma.service.spec.ts
+++ src/prisma/prisma.service.spec.ts
@@ -2,17 +2,33 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from './prisma.service';
 
 describe('PrismaService', () => {
-  let service: PrismaService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [PrismaService],
     }).compile();
 
-    service = module.get<PrismaService>(PrismaService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  afterEach(async () => {
+    await prismaService.$disconnect();
+  });
+
+  it('should connect to the database on module initialization', async () => {
+    const connectSpy = jest.spyOn(prismaService, '$connect');
+
+    await prismaService.onModuleInit();
+
+    expect(connectSpy).toHaveBeenCalled();
+  });
+
+  it('should return order repo', async () => {
+    Reflect.set(prismaService, 'orderUser', 'orderUser');
+
+    const repo = prismaService.getOrderItemRepo();
+
+    expect(repo).toBe('orderUser');
   });
 });
